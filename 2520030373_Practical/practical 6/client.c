ecc16aa6632a6a87b3bae347e91895fecd4fb439
yasasri366 <yasasri.366@gmail.com>
2026-09-24 06:23:18 +0530

    Update client.c

--- 2520030373_Practical/practical 6/client.c
+++ 2520030373_Practical/practical 6/client.c
@@ -5,15 +5,12 @@
 int main()
 {
 char message[100];
-int fd1, fd2;
-fd1 = open("client_fifo", O_WRONLY);
+int fd;
+fd = open("myfifo", O_RDWR);
 printf("Enter message: ");
 fgets(message, sizeof(message), stdin);
-write(fd1, message, strlen(message) + 1);
-close(fd1);
-fd2 = open("server_fifo", O_RDONLY);
-read(fd2, message, sizeof(message));
+write(fd, message, strlen(message) + 1);
+read(fd, message, sizeof(message));
 printf("Server says: %s\n", message);
-close(fd2);
-return 0;
+close(fd);
 }
