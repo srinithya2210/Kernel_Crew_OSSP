0c79102070fb72a50abec075c15dd3a4da243719
jahnavigoka <jahnavigoka@gmail.com>
2026-09-24 08:11:31 +0530

    Implement process creation and synchronization

--- 2520030483_Practical/Practical 4/Waitex.c
+++ 2520030483_Practical/Practical 4/Waitex.c
@@ -1,1 +1,21 @@
-
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/wait.h>
+#include<stdlib.h>
+int main(){
+int pid[3];
+for(int i=0;i<3;i++){
+pid[i]=fork();
+if(pid[i]==0){
+printf("Child %d pid=%d\n",i+1,getpid());
+sleep(i+1);
+exit(0);
+}
+}
+wait(NULL);
+printf("Parent: One child is completed with wait()\n");
+waitpid(pid[2],NULL,0);
+printf("Parent: Child 3 had completed with waitpid()\n");
+wait(NULL);
+printf("Parent:All childs are completed\n");
+}
