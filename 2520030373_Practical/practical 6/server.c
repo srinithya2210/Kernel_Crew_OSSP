9ea3c619ee7b5ea26d22fede1613bf7542e530f4
yasasri366 <yasasri.366@gmail.com>
2026-09-24 06:23:40 +0530

    Update server.c

--- 2520030373_Practical/practical 6/server.c
+++ 2520030373_Practical/practical 6/server.c
@@ -7,17 +7,13 @@
 int main()
 {
 char message[100];
-int fd1, fd2;
-mkfifo("client_fifo", 0666);
-mkfifo("server_fifo", 0666);
+int fd;
+mkfifo("myfifo", 0666);
 printf("Server waiting for client...\n");
-fd1 = open("client_fifo", O_RDONLY);
-read(fd1, message, sizeof(message));
+fd = open("myfifo", O_RDWR);
+read(fd, message, sizeof(message));
 printf("Client says: %s\n", message);
-close(fd1);
 strcpy(message, "Message received by server");
-fd2 = open("server_fifo", O_WRONLY);
-write(fd2, message, strlen(message) + 1);
-close(fd2);
-return 0;
+write(fd, message, strlen(message) + 1);
+close(fd);
 }
