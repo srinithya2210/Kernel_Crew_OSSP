3ac2c75d5aac29460befbaafaca3e1ed51eae475
jahnavigoka <jahnavigoka@gmail.com>
2026-09-24 08:14:38 +0530

    Refactor Zombie.c for better process handling
    Added exit call for child process and improved formatting.

--- 2520030483_Practical/Practical 4/Zombie.c
+++ 2520030483_Practical/Practical 4/Zombie.c
@@ -1,18 +1,19 @@
 #include<stdio.h>
 #include<unistd.h>
 #include<stdlib.h>
-#include<sys/wait.h>
-int main(){
-int pid;
-pid=fork();
-if(pid==0){
-printf("Child process %d\n",getpid());
+int main()
+{
+int  pid;
+pid = fork();
+if(pid==0)
+{
+printf("Child process %d\n", getpid());
 exit(0);
 }
-else{
-printf("Parent process %d\n",getpid());
-//sleep(30);
-wait(NULL);
+else
+{
+printf("parent process %d\n",getpid());
+sleep(30);
 printf("Parent process completed\n");
 }
 }
