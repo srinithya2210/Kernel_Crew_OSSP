1e9a36794ea478bfe34b526aaa8dfa5f7d7ca9e2
yasasri366 <yasasri.366@gmail.com>
2026-09-24 06:13:58 +0530

    Create server.c

---
+++ 2520030373_Practical/practical 6/server.c
@@ -0,0 +1,20 @@
+//server code
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+int main()
+{
+char message[100];
+int fd;
+mkfifo("myfifo", 0666);
+printf("Server waiting for client...\n");
+fd = open("myfifo", O_RDWR);
+read(fd, message, sizeof(message));
+printf("Client says: %s\n", message);
+strcpy(message, "Message received by server");
+write(fd, message, strlen(message) + 1);
+close(fd);
+}
