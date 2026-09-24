7f6f1426b40a98dd2f8567e68b9d28b29169b9c6
jahnavigoka <jahnavigoka@gmail.com>
2026-09-24 10:58:41 +0530

    Update client.c

--- 2520030483_Practical/Practical 6/client.c
+++ 2520030483_Practical/Practical 6/client.c
@@ -1,1 +1,16 @@
-
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+int main()
+{
+char message[100];
+int fd;
+fd = open("myfifo", O_RDWR);
+printf("Enter message: ");
+fgets(message, sizeof(message), stdin);
+write(fd, message, strlen(message) + 1);
+read(fd, message, sizeof(message));
+printf("Server says: %s\n", message);
+close(fd);
+}
